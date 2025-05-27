21a6f9c131687cf3655bb2c047afbf45131a43d1
Nicolas-Barraza-Estrada <nicolas.barraza1901@alumnos.ubiobio.cl>
2025-05-27 10:16:15 -0400

    p

---
+++ backend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({"message": "Chao Mundo desde Django REST"})
